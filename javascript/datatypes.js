fa2c12b55e4d39092006d0dbb8350dc7ac5050cd
AdnanSaedC <adnansaed180@gmail.com>
2024-09-17 07:25:48 +0000

    changes in datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,54 @@
+let value=3;
+console.log(-value);
+
+//power
+console.log(2**4);
+
+//string concat
+let str1="Adnan ";
+let str2="Saed";
+console.log(str1+str2);
+
+//precedence order
+console.log(2+2+"5")
+//first number means will be added
+console.log('2'+2+5)
+//first string means everything will
+//be considered as string
+console.log('2'+(5+5))
+
+//boolean incrementation
+console.log(true);
+console.log(+true);//1 got incremented
+//since highest was one it remained same
+console.log(-true);
+let x=true;
+x--;
+console.log(x);
+
+console.log(+"");
+//it will print zero
+
+console.log(2!=1);
+console.log("2"==2);
+console.log("hi");
+console.log("2"===2);
+console.log("hi");
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+console.log("hi");
+//so it is better to avoid comparison
+//btwn different datatypes
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+
+
+
+
+
+
